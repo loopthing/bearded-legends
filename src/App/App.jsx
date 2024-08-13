2c6fb44e625ddf30895ea695fe487918c5e08091
Loopthing <loopthing8@gmail.com>
2024-08-13 19:45:46 -0400

    Add swipe-down-to-reload.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,15 +4,23 @@ import useServiceWorker from '@hooks/useServiceWorker';
 import * as Layout from '@styles/Layout.scss';
 import Arrays from '@utils/Arrays';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import About from './About';
 import * as Styles from './App.scss';
+import useSwipe from '../hooks/useSwipe';
 
 export default function App({ className }) {
-  const b = useContentBundle(content);
+  const _b = useContentBundle(content);
 
   useServiceWorker();
+  const [swipeRef, swipe] = useSwipe();
+
+  useEffect(() => {
+    if (swipe === 'down') {
+      window.location.reload();
+    }
+  }, [swipe]);
 
   return (
     <div
@@ -23,6 +31,7 @@ export default function App({ className }) {
         Layout.JustifyStart,
         Layout.AlignCenter,
       ).join(' ')}
+      ref={swipeRef}
     >
       <About />
     </div>
