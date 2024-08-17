76686e688a684e61d64f5649fb34c04220c226f4
Loopthing <loopthing8@gmail.com>
2024-08-17 18:57:26 -0400

    Using useNavigate for reload.

--- src/hooks/useCheckUpdates.jsx
+++ src/hooks/useCheckUpdates.jsx
@@ -1,11 +1,13 @@
 import { useLocalStorage } from '@hooks/useStorage';
 import Logger from '@utils/Logger';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function useCheckUpdates() {
   const _logger = new Logger('useCheckUpdates');
   const [appVersion, setAppVersion] = useLocalStorage('BL.App.Version');
   const [availableVersion, setAvailableVersion] = useState(null);
+  const navigate = useNavigate();
 
   async function checkAppUpdates() {
     return fetch('/bearded-legends/package.json')
@@ -18,12 +20,11 @@ export default function useCheckUpdates() {
       });
   }
 
-  async function updateApp(version) {
-    setAppVersion(version);
-
-    return new Promise((resolve) => {
-      resolve(window.location.reload());
-    }, 0);
+  async function updateApp() {
+    setAppVersion(availableVersion);
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    navigate('/', { replace: true });
+    // window.location.reload();
   }
 
   return [appVersion, availableVersion, { checkAppUpdates, updateApp }];
