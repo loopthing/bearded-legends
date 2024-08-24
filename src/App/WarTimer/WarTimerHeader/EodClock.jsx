5d15dbda8bc0d568329c214ecbd5d5d89d9e9727
Loopthing <loopthing8@gmail.com>
2024-08-24 11:29:28 -0400

    Add EOD clock and the logo to the War Timer header. For now, the EOD clock is the preferred clock in XS, but we should make this configurable via Settings screen.

--- src/App/WarTimer/WarTimerHeader/EodClock.jsx
+++ src/App/WarTimer/WarTimerHeader/EodClock.jsx
@@ -6,30 +6,51 @@ import React, { useContext, useEffect, useState } from 'react';
 import * as Styles from './UtcClock.scss';
 import { HeartbeatContext } from '../../HeartbeatProvider';
 
-export default function UtcClock({ className }) {
-  const _logger = new Logger('UtcClock');
+export default function EodClock({ className }) {
+  const _logger = new Logger('EodClock');
   const b = useContentBundle(content);
+  const [isPastEod, setIsPastEod] = useState(true);
   const [hours, setHours] = useState('0');
   const [minutes, setMinutes] = useState('00');
+  const [seconds, setSeconds] = useState('00');
   const { tick } = useContext(HeartbeatContext);
 
   useEffect(() => {
     const now = new Date();
-    const _hours = now.getUTCHours();
-    const _minutes = now.getUTCMinutes();
+    let _hours = now.getUTCHours() - 12;
+    let _minutes = now.getUTCMinutes();
+    let _seconds = now.getUTCSeconds();
+    let _isPastEod = _hours >= 0;
 
+    if (!_isPastEod) {
+      _hours = Math.abs(_hours);
+      _minutes = 59 - _minutes;
+      _seconds = 59 - _seconds;
+    }
+
+    setIsPastEod(_isPastEod);
     setHours(String(_hours));
     setMinutes(String(_minutes).padStart(2, '0'));
+    setSeconds(String(_seconds).padStart(2, '0'));
   }, [tick]);
 
   return (
     <div className={Arrays.pack(className, Styles.Clock).join(' ')}>
       <div className={Styles.Time}>
         <div className={Styles.TimeDisplay}>
-          <b.CLOCK_FORMAT_SHORT hours={hours} minutes={minutes} />
           <span className={Styles.TimeZone}>
-            <b.UTC />
+            <b.EOD />
+            {isPastEod ? b.PlusSign() : b.MinusSign()}
           </span>
+          {isPastEod ? (
+            <b.CLOCK_FORMAT_SHORT hours={hours} minutes={minutes} />
+          ) : (
+            <b.CLOCK_FORMAT_LONG
+              hours={hours}
+              minutes={minutes}
+              seconds={seconds}
+            />
+          )}
         </div>
       </div>
     </div>
--- src/App/WarTimer/WarTimerHeader/WarTimerHeader.jsx
+++ src/App/WarTimer/WarTimerHeader/WarTimerHeader.jsx
@@ -1,40 +1,27 @@
 import content from '@content/Content.yaml';
 import useContentBundle from '@hooks/useContentBundle';
 import * as Layout from '@styles/Layout.scss';
-import Arrays from '@utils/Arrays';
 import Logger from '@utils/Logger';
 import React from 'react';
 import * as Styles from './WarTimerHeader.scss';
-import Clock from './UTCClock';
+import UtcClock from './UtcClock';
+import EodClock from './EodClock';
+import Logo from '../../Logo';
 
-export default function WarTimerHeader({ tick }) {
+export default function WarTimerHeader() {
   const _logger = new Logger('WarTimerHeader');
   const b = useContentBundle(content);
 
   return (
-    <div
-      className={Arrays.pack(
-        Styles.WarTimerHeader,
-        Layout.Flex,
-        Layout.JustifyCenter,
-        Layout.AlignCenter,
-      ).join(' ')}
-    >
-      <div>
-        <div
-          className={Arrays.pack(
-            Layout.Flex,
-            Layout.JustifyCenter,
-            Layout.AlignCenter,
-            Layout.NoWrap,
-          ).join(' ')}
-        >
-          <Clock className={Styles.Clock} tick={tick} />
-        </div>
-        <div className={Layout.TextCenter}>
-          <b.GuildNameDecorative />
-        </div>
+    <>
+      <div className={Styles.WarTimerHeader}>
+        <EodClock className={Styles.Left} />
+        <Logo className={Styles.Center} />
+        <UtcClock className={Styles.Right} />
       </div>
-    </div>
+      <div className={Layout.TextCenter}>
+        <b.GuildNameDecorative />
+      </div>
+    </>
   );
 }
