3901a0ebc2012311774707d54189d612e0e9be00
Loopthing <loopthing8@gmail.com>
2024-08-24 12:38:20 -0400

    Simplify the EOD clock to only show the countdown. This should match the in-game countdown clock.

--- src/App/WarTimer/WarTimerHeader/EodClock.jsx
+++ src/App/WarTimer/WarTimerHeader/EodClock.jsx
@@ -9,48 +9,64 @@ import { HeartbeatContext } from '../../HeartbeatProvider';
 export default function EodClock({ className }) {
   const _logger = new Logger('EodClock');
   const b = useContentBundle(content);
-  const [isPastEod, setIsPastEod] = useState(true);
-  const [hours, setHours] = useState('0');
-  const [minutes, setMinutes] = useState('00');
-  const [seconds, setSeconds] = useState('00');
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
   const { tick } = useContext(HeartbeatContext);
 
   useEffect(() => {
     const now = new Date();
-    let _hours = now.getUTCHours() - 12;
+    let _hours = now.getUTCHours();
     let _minutes = now.getUTCMinutes();
     let _seconds = now.getUTCSeconds();
-    let _isPastEod = _hours >= 0;
 
-    if (!_isPastEod) {
-      _hours = Math.abs(_hours);
-      _minutes = 59 - _minutes;
-      _seconds = 59 - _seconds;
+    if (_hours === 24) {
+      _hours = 0;
     }
 
-    setIsPastEod(_isPastEod);
-    setHours(String(_hours));
-    setMinutes(String(_minutes).padStart(2, '0'));
-    setSeconds(String(_seconds).padStart(2, '0'));
+    _hours = 11 - _hours;
+    _minutes = 59 - _minutes;
+    _seconds = 59 - _seconds;
+
+    if (_hours < 0) {
+      _hours += 24;
+    }
+
+    setHours(_hours);
+    setMinutes(_minutes);
+    setSeconds(_seconds);
   }, [tick]);
 
   return (
     <div className={Arrays.pack(className, Styles.Clock).join(' ')}>
       <div className={Styles.Time}>
         <div className={Styles.TimeDisplay}>
-          <span className={Styles.TimeZone}>
+          {/* <span className={Styles.TimeZone}>
             <b.EOD />
-            {isPastEod ? b.PlusSign() : b.MinusSign()}
-          </span>
-          {isPastEod ? (
-            <b.CLOCK_FORMAT_SHORT hours={hours} minutes={minutes} />
-          ) : (
-            <b.CLOCK_FORMAT_LONG
-              hours={hours}
-              minutes={minutes}
-              seconds={seconds}
-            />
+            <b.MinusSign />
+          </span> */}
+          {!!hours && (
+            <>
+              {hours}
+              <abbr title={b.Hours()}>
+                <b.HoursAbbr />
+              </abbr>
+            </>
           )}
+          {(!!hours || !!minutes) && (
+            <>
+              {minutes}
+              <abbr title={b.Minutes()}>
+                <b.MinutesAbbr />
+              </abbr>
+            </>
+          )}
+          <span className={Styles.Seconds}>
+            {seconds}
+            <abbr title={b.Seconds()}>
+              <b.SecondsAbbr />
+            </abbr>
+          </span>
         </div>
       </div>
     </div>
