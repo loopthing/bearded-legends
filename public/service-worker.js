20eab3c3931d8e22cdd61447cfee1c263da4fc3f
Loopthing <loopthing8@gmail.com>
2024-09-14 09:29:26 -0400

    Software update button is not working as expected -- Disable bundle caching until we figure this out.

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ const ASSETS = [
   '/',
   '/favicon.ico',
   '/index.html',
-  '/bundle.js',
+  // '/bundle.js',
   '/manifest.json',
   '/icons/android-chrome-192x192.png',
   '/icons/android-chrome-512x512.png',
